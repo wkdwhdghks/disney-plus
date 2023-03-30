a98a85f2914917d5d5b48a3bcc30975d02ed693c
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-03-30 22:34:52 +0900

    [Feat] 이미지 배너 생성

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,21 @@
 import { useState, useEffect } from "react";
 import axios from "../api/axios";
 import requests from "../api/request";
+import "./Banner.css";
 
 export default function Banner(): JSX.Element {
-  const [movie, setMovie] = useState([]);
+  interface MovieInfo {
+    backdrop_path: string;
+    title: string;
+    name: string;
+    original_name: string;
+    videos: {
+      results: [{ key: string }];
+    };
+    overview: string;
+  }
+
+  const [movie, setMovie] = useState<MovieInfo>();
 
   useEffect(() => {
     fetchData();
@@ -21,6 +33,38 @@ export default function Banner(): JSX.Element {
     setMovie(movieDetail);
   };
 
-  console.log(movie);
-  return <div>h2</div>;
+  const truncate = (str: string | undefined, n: number) => {
+    if (typeof str === "string") {
+      return str?.length > n ? str.substr(0, n) + "..." : str;
+    }
+  };
+
+  return (
+    <header
+      className="banner"
+      style={
+        movie !== undefined
+          ? {
+              backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+              backgroundPosition: "top center",
+              backgroundSize: "cover",
+            }
+          : {}
+      }
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+
+        <div className="banner__buttons">
+          {movie?.videos?.results[0]?.key && (
+            <button className="banner__button play">play</button>
+          )}
+        </div>
+        <p className="banner__description">{truncate(movie?.overview, 100)}</p>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
 }
