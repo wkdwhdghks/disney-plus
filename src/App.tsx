899f521df7a2ed68005b7f9743364cad3e02becb
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-03-30 19:20:09 +0900

    [Feat] 비디오 배너 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import Nav from "./components/Nav";
+import styled from "styled-components";
 import "./App.css";
 
 export default function App() {
   return (
-    <div>
+    <Container>
       <Nav />
-    </div>
+    </Container>
   );
 }
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &::after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
