e1868876aee1096b3c2a5bf9a0f32201ba107d28
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-04-04 15:43:18 +0900

    [Feat] 검색 페이지 ui구현

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,9 +1,15 @@
 import axios from "../../api/axios";
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
+import "./Search.css";
 
 export default function Search() {
-  const [searchResults, setSearchResults] = useState([]);
+  interface MovieInfo {
+    backdrop_path: string;
+    media_type: string;
+    id: number;
+  }
+  const [searchResults, setSearchResults] = useState<MovieInfo[]>([]);
 
   const useQuery = () => {
     return new URLSearchParams(useLocation().search);
@@ -11,6 +17,7 @@ export default function Search() {
 
   let query = useQuery();
   const searchTerm = query.get("q");
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (searchTerm) {
@@ -23,11 +30,48 @@ export default function Search() {
       const response = await axios.get(
         `search/multi?include_adult=false&query=${searchTerm}`
       );
-      setSearchResults(response.data.result);
+      setSearchResults(response.data.results);
     } catch (error) {
       console.log(error);
     }
   };
 
-  return <div>Search</div>;
+  if (searchResults?.length > 0) {
+    return (
+      <section className="search-container">
+        {searchResults.map((movie: MovieInfo) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return (
+              <div className="movie" key={movie.id}>
+                <div
+                  className="movie__column-poster"
+                  onClick={() => {
+                    navigate(`${movie.id}`);
+                  }}
+                >
+                  <img
+                    src={movieImageUrl}
+                    alt="movie"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          } else {
+            return "";
+          }
+        })}
+      </section>
+    );
+  } else {
+    return (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>찾고자하는 검색어 "{searchTerm}" 에 맞는 영화가 없습니다.</p>
+        </div>
+      </section>
+    );
+  }
 }
